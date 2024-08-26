bdc8a5ea1be296cfc4c7738fe383752010fc05d0
yeison rolon <yeisonrolon117@gmail.com>
2024-08-25 21:42:14 -0500

    avance de hu1

---
+++ src/main/java/bootcamp_pragma/microservicio_stock/MicroservicioStockApplication.java
@@ -0,0 +1,13 @@
+package bootcamp_pragma.microservicio_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootcamp_pragma/microservicio_stock/MicroservicioStockApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp_pragma.microservicio_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
